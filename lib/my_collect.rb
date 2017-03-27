e1ee732e03eb17123c03df9f655a424eba3b730b
Paisley Singh <paisley.singh@gmail.com>
2017-03-27 19:35:40 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def my_collect (collection)
+  counter = 0
+  result = []
 
+  while counter < collection.length
+    result.push(yield (collection[counter]))
+    counter += 1
+  end
 
+  result
+end
